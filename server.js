ecb3abf51f7dfbc0f8fb0f7f6fe316151a33a9ab
jc-myeasypay <jc@easypaymentsplus.com>
2017-09-03 20:14:33 +0100

    set up start script and heruko port

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var express = require('express')
 var hbs = require('hbs')
 var app = express()
 const fs = require('fs')
+const port = process.env.PORT || 3000
+
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs')
 
@@ -59,6 +61,6 @@ app.get('/about', (req, res) => {
   })
 })
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000')
+app.listen(port, () => {
+  console.log(`Server is up on port : ${port}`)
 })
